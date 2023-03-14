1087ff431ad10c228605a886490533c693346978
klozz <carlosj@klozz.dev>
2023-03-14 05:10:45 +0000

    X2: Build libprotobuf-cpp-lite required by widevine 1.3
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -159,7 +159,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+	libprotobuf-cpp-lite
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
